aca194338269147f38a3c76028f03e3b69c87b5e
Adolfo Olguin <adokurt@gmail.com>
2015-10-23 19:49:20 -0200

    update JobListAcitity class

--- app/src/main/java/com/akomsoft/findjob/JobListActivity.java
+++ app/src/main/java/com/akomsoft/findjob/JobListActivity.java
@@ -2,21 +2,70 @@ package com.akomsoft.findjob;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class JobListActivity extends AppCompatActivity {
+public class JobListActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String JSON_URL = "http://akomsoft.com/WebServer/getData.php";
+
+    private Button buttonGet;
+
+    private ListView listView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_list);
+        buttonGet = (Button) findViewById(R.id.buttonGet);
+        buttonGet.setOnClickListener(this);
+        listView = (ListView) findViewById(R.id.listView);
 
+    }
+    private void sendRequest(){
+
+        StringRequest stringRequest = new StringRequest(JSON_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        showJSON(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(JobListActivity.this,error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private void showJSON(String json){
+        ParseJSON pj = new ParseJSON(json);
+        pj.parseJSON();
+        CustomList cl = new CustomList(this, ParseJSON.ids,ParseJSON.tittles,ParseJSON.locations,ParseJSON.companies);
+        listView.setAdapter(cl);
+    }
+
+    @Override
+    public void onClick(View v) {
+        sendRequest();
     }
 
 }
