ce6c75902b1d814e715674893de0e5ee5729552b
Adolfo Olguin <adokurt@gmail.com>
2015-10-23 19:35:50 -0200

    crated CustomList class

---
+++ app/src/main/java/com/akomsoft/findjob/CustomList.java
@@ -0,0 +1,43 @@
+package com.akomsoft.findjob;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class CustomList extends ArrayAdapter<String> {
+    private String[] ids;
+    private String[] tittles;
+    private String[] locations;
+    private String[] companies;
+    private Activity context;
+
+    public CustomList(Activity context, String[] ids, String[] tittles,
+                      String[] locations, String[] companies) {
+        super(context, R.layout.list_job, ids);
+        this.context = context;
+        this.ids = ids;
+        this.tittles = tittles;
+        this.locations = locations;
+        this.companies = companies;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View listViewItem = inflater.inflate(R.layout.list_job, null, true);
+        TextView textViewId = (TextView) listViewItem.findViewById(R.id.texViewId);
+        TextView textViewTittle = (TextView) listViewItem.findViewById(R.id.textViewTitle);
+        TextView textViewLocation = (TextView) listViewItem.findViewById(R.id.textViewLocation);
+        TextView textViewCompany = (TextView) listViewItem.findViewById(R.id.textViewCompany);
+
+        textViewId.setText(ids[position]);
+        textViewTittle.setText(tittles[position]);
+        textViewLocation.setText(locations[position]);
+        textViewCompany.setText(companies[position]);
+
+        return listViewItem;
+    }
+}
--- app/src/main/java/com/akomsoft/findjob/JobListActivity.java
+++ app/src/main/java/com/akomsoft/findjob/JobListActivity.java
@@ -2,12 +2,21 @@ package com.akomsoft.findjob;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class JobListActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job_list);
+
     }
+
 }
